398a0a99485038a3e7c37e6d03e8d2e058ec21e8
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-05-23 12:31:29 +0200

    Finished 13.8

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
 var fs = require('fs');
-var StatMode = require('stat-mode');
-var colors = require('colors');
-var path = require('path');
+var http = require('http');
+var server = http.createServer();
 
-// fs.stat('./cats.jpg', function(err, stats) {
-//     var statMode = new StatMode();
-//     console.log(statMode(stats));
-// });
+server.on ('request', (function(req, res) {
+    if (req.method === 'GET' && req.url === "/") {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        file = fs.createReadStream('./index.html');
+        file.pipe(res);
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'image/png');
+        file = fs.createReadStream('./404.png');
+        file.pipe(res);
+    }
+}));
 
-// fs.readFile('./text.txt', 'utf8', function(err, data) {
-//     console.log('Dane przed zapisem!'.blue);
-//     console.log(data);
-//     fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function(err) {
-//         if (err) throw err; 
-//         console.log('Zapisano!'.blue);
-//         fs.readFile('./text.txt', 'utf-8', function(err, data) {
-//             console.log('Dane po zapisie'.blue);
-//             console.log(data);
-//         });
-//     });
-// });
 
-function listDirectory (dir, file) {
-    fs.readdir(dir, function(err, files) {
-        if (err) throw err;
-    
-        fs.writeFile(path.join(__dirname, file), 'Zawartość odczytanego katalogu:', function(err, data) {
-            if (err) throw err;        
-        });
-    
-        files.forEach(function(filename) {
-            fs.appendFile(path.join(__dirname, file), '\n- ' + filename, function(err, data) {
-                if (err) throw err; 
-            });
-        });
-    
-        console.log('Odczytano zawartość katalogu i zapisano do pliku readdir.txt'.blue);
-    });
-}
-
-listDirectory('./', 'readdir.txt');
+server.listen(8080, function() {
+    console.log('Serwer uruchomiony pod adresem: http://localhost:8080');
+});
